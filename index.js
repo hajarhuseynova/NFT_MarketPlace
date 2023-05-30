88e0521253691c4b25f30ec16292f97a6e95fca9
hajar298 <hajarih@code.edu.az>
2023-05-30 11:40:44 +0400

    add listeners

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const hamburgerMenu = document.getElementById("hamburgerMenu");
+const registerButton = document.getElementById("registerButton");
 const sidebar = document.querySelector(".sidebar");
+const navbarLeft = document.querySelector(".navbar_left");
+const rankingButton = document.querySelector("#rankingButton");
+const seeAllButton = document.querySelector("#seeAllButton");
+const creatorCard=document.querySelectorAll(".creator_card")
 hamburgerMenu.addEventListener("click", () => {
   sidebar.classList.toggle("visible");
+});
+registerButton.addEventListener("click", () => {
+  window.location.href = "./register.html";
+});
+navbarLeft.addEventListener("click", () => {
+  window.location.href = "./index.html";
+});
+rankingButton.addEventListener("click", () => {
+  window.location.href = "./rankings.html";
+});
+seeAllButton.addEventListener("click", () => {
+  window.location.href = "./marketplace.html";
+});
+creatorCard.forEach(element => {
+  element.addEventListener("click",()=>{
+    window.location.href = "./artist.html";
+  });
 });
